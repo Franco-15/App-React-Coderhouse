1f92b33ae3431676d26cecf3b0c92c8964bccea1
Franco-15 <francomante1510@gmail.com>
2022-12-01 19:03:00 -0300

    Cambio componentes de app original

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/Navbar';
+import ItemListContainer from './components/ItemListContainer';
+
+/*BOOSTRAP */
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="app">
+      <header>
+        <NavBar/>
       </header>
+      <main>
+        <ItemListContainer greeting='Bienvenidos a MDQ Bebidas!'/>
+      </main>
     </div>
   );
 }
