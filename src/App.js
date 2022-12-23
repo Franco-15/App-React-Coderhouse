c20233c5fa36b5a33a2da21972f2a325bf0784ed
Franco-15 <francomante1510@gmail.com>
2022-12-23 01:16:14 -0300

    actualizo cambios en app.js

--- src/App.js
+++ src/App.js
@@ -10,20 +10,17 @@ import data from './productos.json';
 
 function App() {
   const drinks = data;
-  const categories = drinks.map((drink) => drink.tipo);
-  const categoriesUnique = [...new Set(categories)];
 
   return (
     <div className="app">
       <header>
-        <NavBar/>
+        <NavBar drinks={drinks}/>
       </header>
       <main>
-        <h1>{'Bienvenidos a MDQ Bebidas!'}</h1>
         <Routes>
           <Route path='/' element={<ItemListContainer drinks={drinks}/>}/>
           <Route path='/category/:categoryId' element={<ItemListContainer drinks={drinks}/>}/>
-          {/* <Route path='/item/:itemId' element={<ItemDetailContainer drinks={drinks}/>}/> */}
+          <Route path='/item/:itemId' element={<ItemDetailContainer drinks={drinks}/>}/>
         </Routes>
       </main>
     </div>
