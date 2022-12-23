a7d2739419eb0ddc7525291f3a02c4699c757e0d
Franco-15 <francomante1510@gmail.com>
2022-12-23 01:18:09 -0300

    Actualizo barra de nav

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,29 +3,34 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import CartWidget from './CartWidget';
-import {Link}from "react-router-dom"
+import { Link } from "react-router-dom"
 
-function NavBar(){
-    return (
-        <>
-          <Navbar bg="dark" variant="dark">
-            <Container>
-              <Navbar.Brand href=""><img className='logo' src='https://res.cloudinary.com/dgk9fa4rw/image/upload/v1669930264/ImgAppReact/img/MDQ_Bebidas-r_atpaka.png' alt='logo'/></Navbar.Brand>
-              <Nav className="me-auto">
-                <Link to="vinos">Vinos</Link>
-                <Link to="cervezas">Cervezas</Link>
-                <Link to="aperitivos">Aperitivos</Link>
-                <Link to="bebidasBlancas">Bebidas Blancas</Link>
-                <Link to="energizantes">Energizantes</Link>
-                <Link to="sinAlcohol">Sin Alcohol</Link>
-              </Nav>
-              <Nav>
-                <Nav.Link href=""><CartWidget/></Nav.Link>
-              </Nav>
-            </Container>
-          </Navbar>
-        </>
-      );
+function NavBar({ drinks }) {
+
+  const categoriesRepeat = drinks.map((drink) => drink.tipo);
+  const categories = [...new Set(categoriesRepeat)];
+
+  return (
+    <>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Link to='/'>
+            <Navbar.Brand href=""><img className='logo' src='https://res.cloudinary.com/dgk9fa4rw/image/upload/v1669930264/ImgAppReact/img/MDQ_Bebidas-r_atpaka.png' alt='logo' /></Navbar.Brand>
+          </Link>
+          <Nav className="me-auto">
+            {categories.map((category) => {
+              return (
+                <Link className='catLinks' key={category} to={`/category/${category}`}>{category}</Link>
+              )
+            })}
+          </Nav>
+          <Nav>
+            <Nav.Link href=""><CartWidget /></Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    </>
+  );
 }
 
 export default NavBar;
