93b3405cbbd633c93f5da3afc11ddb738e930eea
Franco-15 <francomante1510@gmail.com>
2023-01-17 16:36:17 -0300

    change navbar to responsive navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,35 +1,50 @@
-import './Navbar.css';
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import CartWidget from './CartWidget';
-import { Link } from "react-router-dom"
+import "./Navbar.css";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import CartWidget from "./CartWidget";
+import { Link } from "react-router-dom";
 
 function NavBar({ drinks }) {
-
   const categoriesRepeat = drinks.map((drink) => drink.tipo);
   const categories = [...new Set(categoriesRepeat)];
 
   return (
-    <>
-      <Navbar bg="dark" variant="dark">
-        <Container>
-          <Link to='/'>
-            <Navbar.Brand href=""><img className='logo' src='https://res.cloudinary.com/dgk9fa4rw/image/upload/v1669930264/ImgAppReact/img/MDQ_Bebidas-r_atpaka.png' alt='logo' /></Navbar.Brand>
-          </Link>
+    <Navbar expand="lg" bg="dark" variant="dark">
+      <Container>
+        <Link to="/">
+          <Navbar.Brand href="">
+            <img
+              className="logo"
+              src="https://res.cloudinary.com/dgk9fa4rw/image/upload/v1669930264/ImgAppReact/img/MDQ_Bebidas-r_atpaka.png"
+              alt="logo"
+            />
+          </Navbar.Brand>
+        </Link>
+        
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
             {categories.map((category) => {
               return (
-                <Link className='catLinks' key={category} to={`/category/${category}`}>{category}</Link>
-              )
+                <Link
+                  className="catLinks"
+                  key={category}
+                  to={`/category/${category}`}
+                >
+                  {category}
+                </Link>
+              );
             })}
           </Nav>
-          <Nav>
-            <Nav.Link href=""><CartWidget /></Nav.Link>
-          </Nav>
-        </Container>
-      </Navbar>
-    </>
+          <Nav className="navbarCartWidget">
+          <Nav.Link href="">
+            <CartWidget />
+          </Nav.Link>
+        </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 }
 
