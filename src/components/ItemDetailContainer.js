1846ad56c91bd74706f2c777762edff2ef640841
Franco-15 <francomante1510@gmail.com>
2023-01-17 16:44:34 -0300

    add incr/decr button

--- src/components/ItemDetailContainer.js
+++ src/components/ItemDetailContainer.js
@@ -4,14 +4,26 @@ import { useParams } from "react-router-dom";
 import Button from 'react-bootstrap/Button';
 import './ItemDetailContainer.css';
 import { Container } from 'react-bootstrap';
+import { useEffect, useState } from 'react';
 
 function ItemDetailContainer({ drinks }) {
+    const [cantidad, setCantidad] = useState(1)
+    
+    const incrCant = () => setCantidad(cantidad + 1)
+    const decrCant = () => setCantidad(cantidad - 1)
+
+    useEffect(()=>{
+        setCantidad(1);
+    },[])
+
     let itemId = useParams();
     const item = drinks.find((drink) => drink.id === itemId.itemId);
     return (
         <div className='bodyDetail'>
             <Card className='cardDetail'>
-                <Card.Img className='imgDetail' variant="top" src={item.img.src} alt={item.img.alt} />
+                <div className='imgBox'>
+                    <Card.Img className='imgDetail' variant="top" src={item.img.src} alt={item.img.alt} />
+                </div>
                 <Container className='containerDetail'>
                     <Card.Body>
                         <Card.Title>{item.nombre.toUpperCase()}</Card.Title>
@@ -23,6 +35,11 @@ function ItemDetailContainer({ drinks }) {
                         <ListGroup.Item className='listDescriptionItem'>{`Tipo: ${item.tipo.charAt(0).toUpperCase() + item.tipo.slice(1)}`}</ListGroup.Item>
                         <ListGroup.Item className='listDescriptionItem'>{`Precio: $${item.precio}`}</ListGroup.Item>
                     </ListGroup>
+                    <Card.Body className='quantify'>
+                        <Button variant="primary" disabled={cantidad===1} onClick={decrCant}>-</Button>
+                        <div className='logQuantify'>{cantidad}</div>
+                        <Button variant="primary"  onClick={incrCant}>+</Button>
+                    </Card.Body>
                     <Card.Body className='buttonDetail'>
                         <Button variant="primary">Agregar al carrito</Button>
                     </Card.Body>
