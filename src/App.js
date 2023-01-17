4c49ed61ee87e04f784e6a733d709b0ef60c8c26
Franco-15 <francomante1510@gmail.com>
2023-01-17 19:42:27 -0300

    update app component

--- src/App.js
+++ src/App.js
@@ -2,25 +2,40 @@ import './App.css';
 import NavBar from './components/Navbar';
 import ItemListContainer from './components/ItemListContainer';
 import ItemDetailContainer from './components/ItemDetailContainer';
-
 /*BOOSTRAP */
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Route, Routes } from 'react-router-dom';
-import data from './productos.json';
+import CartWidget from './components/CartWidget';
+import {db} from './db/firebase-config';
+import Cart from './components/Cart';
+import { useEffect, useState } from 'react';
+import {collection, getDocs} from 'firebase/firestore';
 
 function App() {
-  const drinks = data;
+  const [cartLength, setcartLength] = useState(0);
+  const [drinks, setDrinks] = useState([]);
+  const collectionRef = collection(db, 'Items');
+
+  async function getProducts(){
+    const data = await getDocs(collectionRef);
+    setDrinks(data.docs.map(doc => ({...doc.data(), id: doc.id})));
+  }
+
+  useEffect(() => {
+    getProducts();
+  },[])
 
   return (
     <div className="app">
       <header>
-        <NavBar drinks={drinks}/>
+        <NavBar drinks={drinks} cartLength={cartLength}/>
       </header>
       <main>
         <Routes>
           <Route path='/' element={<ItemListContainer drinks={drinks}/>}/>
           <Route path='/category/:categoryId' element={<ItemListContainer drinks={drinks}/>}/>
           <Route path='/item/:itemId' element={<ItemDetailContainer drinks={drinks}/>}/>
+          <Route path='/cart' element={<Cart/>}/>
         </Routes>
       </main>
     </div>
