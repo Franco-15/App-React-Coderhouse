a006d06894181f8e35a809fa1e771b4c4996a4e7
Franco-15 <francomante1510@gmail.com>
2023-01-17 19:37:48 -0300

    install and configure firebase

---
+++ src/db/firebase-config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+import {getFirestore} from 'firebase/firestore';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD0yTwY1gkjw5ASzE9lvYmZxyaPjdypCX8",
+  authDomain: "mdq-bebidas-react.firebaseapp.com",
+  projectId: "mdq-bebidas-react",
+  storageBucket: "mdq-bebidas-react.appspot.com",
+  messagingSenderId: "659636376284",
+  appId: "1:659636376284:web:92589588de9b6c2d559a19"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
