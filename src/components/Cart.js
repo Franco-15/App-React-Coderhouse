870cd7ed52bb09e3844a9f225f8fc2dc53064da4
Franco-15 <francomante1510@gmail.com>
2023-01-17 16:44:51 -0300

    create Cart component

---
+++ src/components/Cart.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Cart.css';
+
+function Cart() {
+    return (
+        <div className="cartBody">
+        <h1 className="cartTitle">Carrito</h1>
+        </div>
+    );
+}
+
+export default Cart;
